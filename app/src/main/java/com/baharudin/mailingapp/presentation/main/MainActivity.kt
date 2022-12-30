560745fdd1f2e6b56c4c7f66e8e3c299c2c05954
Baharudin Maulana <baharudinmaulana78@gmail.com>
2022-12-30 14:26:48 +0700

    update home

--- app/src/main/java/com/baharudin/mailingapp/presentation/main/MainActivity.kt
+++ app/src/main/java/com/baharudin/mailingapp/presentation/main/MainActivity.kt
@@ -1,20 +1,58 @@
 package com.baharudin.mailingapp.presentation.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.baharudin.mailingapp.R
 import com.baharudin.mailingapp.core.SharedPrefs
 import com.baharudin.mailingapp.databinding.ActivityMainBinding
+import com.baharudin.mailingapp.presentation.login.LoginActivity
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    lateinit var navigationController : NavController
     @Inject
     lateinit var sharedPrefs: SharedPrefs
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navigation = supportFragmentManager.findFragmentById(R.id.fragment_view) as NavHostFragment
+        navigationController = navigation.findNavController()
+        binding.apply {
+            bottomNavigationView.setupWithNavController(navigationController)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        checkIsLoggedIn()
+    }
+
+    private fun checkIsLoggedIn(){
+        if (sharedPrefs.getToken().isEmpty()){
+            goToLoginActivity()
+        }
+    }
+
+    private fun goToLoginActivity(){
+        startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+        finish()
+    }
+
+    private fun signOut(){
+        sharedPrefs.clear()
+        goToLoginActivity()
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return navigationController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/baharudin/mailingapp/presentation/main/profile/ProfileFragment.kt
+++ app/src/main/java/com/baharudin/mailingapp/presentation/main/profile/ProfileFragment.kt
@@ -1,22 +1,21 @@
-package com.baharudin.mailingapp.presentation.main.home
+package com.baharudin.mailingapp.presentation.main.profile
 
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.baharudin.mailingapp.R
-import com.baharudin.mailingapp.databinding.FragmentHomeBinding
+import com.baharudin.mailingapp.databinding.FragmentProfileBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HomeFragment : Fragment(R.layout.fragment_home) {
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
 
-    private var _binding : FragmentHomeBinding? = null
+    private var _binding : FragmentProfileBinding? = null
     private val binding get() = _binding!!
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        _binding = FragmentHomeBinding.bind(view)
+        _binding = FragmentProfileBinding.bind(view)
         super.onViewCreated(view, savedInstanceState)
-
-
     }
+
 }
