494390023151350a9e994edb3d13f4d802c9446f
Baharudin78 <bencoxgame78@gmail.com>
2023-04-26 15:56:16 +0700

    Revert "add chukker depedency"
    This reverts commit dc2174dc

--- app/src/main/java/com/baharudin/mailingapp/data/common/module/NetworkModule.kt
+++ app/src/main/java/com/baharudin/mailingapp/data/common/module/NetworkModule.kt
@@ -1,13 +1,19 @@
 package com.baharudin.mailingapp.data.common.module
 
+import android.content.Context
 import com.baharudin.mailingapp.core.Constats.BASE_URL
 import com.baharudin.mailingapp.core.SharedPrefs
 import com.baharudin.mailingapp.data.common.utils.RequestInterceptor
+import com.chuckerteam.chucker.api.ChuckerCollector
+import com.chuckerteam.chucker.api.ChuckerInterceptor
+import com.chuckerteam.chucker.api.RetentionManager
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
@@ -26,14 +32,37 @@ object NetworkModule{
         }.build()
     }
 
+
+    @Provides
+    @Singleton
+    fun providesChuckInterceptor(@ApplicationContext context: Context): ChuckerInterceptor {
+        val chuckerCollector = ChuckerCollector(
+            context = context,
+            showNotification = true,
+            retentionPeriod = RetentionManager.Period.ONE_HOUR
+        )
+
+        return ChuckerInterceptor.Builder(context)
+            .collector(chuckerCollector)
+            .maxContentLength(250_000L)
+            .redactHeaders("**")
+            .alwaysReadResponseBody(true)
+            .build()
+    }
+
     @Singleton
     @Provides
-    fun provideOkHttpClient(requestInterceptor: RequestInterceptor) : OkHttpClient{
+    fun provideOkHttpClient(
+        requestInterceptor: RequestInterceptor,
+        chuckerInterceptor: ChuckerInterceptor
+    ) : OkHttpClient{
         return OkHttpClient.Builder().apply {
             connectTimeout(60, TimeUnit.SECONDS)
             writeTimeout(60, TimeUnit.SECONDS)
             readTimeout(60, TimeUnit.SECONDS)
             addInterceptor(requestInterceptor)
+            addInterceptor(chuckerInterceptor)
+            addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
         }.build()
     }
 
